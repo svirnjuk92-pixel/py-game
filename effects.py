5bbd2a05ff035f626dbf2e2ec5a5c1ff15ef8d73
svirnjuk92-pixel <svirnjuk92@gmail.com>
2025-10-24 16:53:42 +0300

    Logika

---
+++ effects.py
@@ -0,0 +1,11 @@
+from pygame import draw
+from setting import black
+
+def draw_key_effect(screen, rect, is_pressed = False):
+    if not is_pressed:
+        base_color = (220,220,220)
+    else:
+        base_color = (170,220,255)
+    border_color = black
+    draw.rect(screen, base_color, rect, border_radius=8)
+    draw.rect(screen, border_color, rect, 2, border_radius=8)
